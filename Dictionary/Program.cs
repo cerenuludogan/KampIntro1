3cccfb906fad9ac6e8448436d0336327c2d1be99
Ceren <zcerenuludogan@gmail.com>
2021-02-17 21:27:43 +0300

    week 4 homework

---
+++ Dictionary/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string,int> sehirler = new MyDictionary<string,int>();
+            sehirler.Add("Antalya", 30000);
+            sehirler.Add("İzmir", 250000);
+
+            Console.WriteLine(sehirler.Lenght);
+
+            foreach (var sehir in sehirler.itemkey)
+            {
+                Console.WriteLine(sehir);
+            }
+
+            foreach (var nufus in sehirler.itemvalue)
+            {
+                Console.WriteLine(nufus);
+            }
+        }
+    }
+}
